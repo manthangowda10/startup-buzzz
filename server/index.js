57fdcad4fbd4b224fab3fc36dddc65123e836d26
Manthan Gowda <manthangwd10@gmail.com>
2024-12-31 11:42:46 +0530

    backend routes -1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello from server");
-})
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+app.use('/user',require('./user/userController'));
+app.use('/admin',require('./admin/adminController'));
+
+app.listen(port,() => {
+    console.log(`Server is running on http://localhost:${port}`);
 })
