f67f6368c32cfc4745e471fb869c6a02100f813f
Manthan Gowda <manthangwd10@gmail.com>
2025-01-22 23:21:49 +0530

    route middleware added

--- server/admin/adminRoutes.js
+++ server/admin/adminRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const viewAppointements = require('./appointements/appointementRoutes');
 const manageServices = require('./services/serviceRoutes');
 const { adminSignUp, adminLogin } = require('./adminController');
+const adminAuthenticate = require('../middleware/adminauthenticate');
 
-router.use('/services', manageServices);
-router.use('/appointements',viewAppointements);
-router.post('/signup',adminSignUp);
-router.post('/login',adminLogin);
+
+
+router.use('/services',adminAuthenticate, manageServices); //admin 
+router.use('/appointements',adminAuthenticate, viewAppointements);
+router.post('/signup',adminSignUp); //Admin signup route
+router.post('/login',adminLogin);//Admin login route
 
 module.exports = router;
--- server/admin/services/serviceRoutes.js
+++ server/admin/services/serviceRoutes.js
@@ -1,9 +1,9 @@
 const express= require('express');
 const router = express.Router();
-const {manageServices,modifyService} = require('./serviceControl');
+const {manageServices,modifyService,deleteService} = require('./serviceControl');
 
 router.post('/manage', manageServices);
-router.put('/modifyService/:id',modifyService)
-
+router.put('/modifyService/:id',modifyService);
+router.delete('/deleteService/:id',deleteService);
 
 module.exports = router;
