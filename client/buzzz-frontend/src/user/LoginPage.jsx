775fabd549a28d341be4ea270bc09bc2c430aa62
Manthan Gowda <manthangwd10@gmail.com>
2025-01-23 22:57:24 +0530

    started frontend

--- client/buzzz-frontend/src/user/LoginPage.jsx
+++ client/buzzz-frontend/src/user/LoginPage.jsx
@@ -1,9 +1,31 @@
-import { TextField } from "@mui/material";
+import { TextField,Button } from "@mui/material";
+import { useState } from "react";
+
 
 const LoginPage = () => {
-    return <div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </div>
+
+    const [email,setEmail] = useState("");
+    const [password,setPasswoord] = useState("");
+
+    const handleLogin = () => {
+        console.log("Email: ",email);
+        console.log("Password: ", password);
+    }
+
+    return(
+         <div style ={{maxWidth:400, margin:"0 auto", padding:"20px"}}>
+            <h2>Login</h2>
+        <TextField 
+        id="email" 
+        label="Email" 
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value ={ email }
+        onChange={(e) => setEmail(e.target.value)}
+        />
+         </div>
+    )
 }
 
 export default LoginPage;
