db2b03b9ba6376de7bdc97db6b2cb73c459c0cb5
Manthan Gowda <manthangwd10@gmail.com>
2025-01-31 22:04:47 +0530

    made changes in routes

--- server/admin/appointements/appointementRoutes.js
+++ server/admin/appointements/appointementRoutes.js
@@ -1,8 +1,9 @@
 const express= require('express');
 const router = express.Router();
 const { viewAppointements } = require('./appointementController');
+const {adminAuthenticate} = require('../../middleware/adminauthenticate');
 
 
-router.get('/view',viewAppointements);
+router.get('/view',adminAuthenticate,viewAppointements);
 
 module.exports = router;
--- server/admin/services/serviceRoutes.js
+++ server/admin/services/serviceRoutes.js
@@ -1,9 +1,10 @@
 const express= require('express');
 const router = express.Router();
 const {manageServices,modifyService,deleteService} = require('./serviceControl');
+const {adminAuthenticate} = require('../../middleware/adminauthenticate');
 
-router.post('/manage', manageServices);
-router.put('/modifyService/:id',modifyService);
-router.delete('/deleteService/:id',deleteService);
+router.post('/manage',adminAuthenticate, manageServices);
+router.put('/modifyService/:id',adminAuthenticate,modifyService);
+router.delete('/deleteService/:id',adminAuthenticate,deleteService);
 
 module.exports = router;
