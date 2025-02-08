bd6e23fa7a21176d30dfe8b11344cc121849ebe5
Manthan Gowda <manthangwd10@gmail.com>
2025-02-08 23:23:53 +0530

    logout

--- client/buzzz-frontend/src/user/Dashboard.jsx
+++ client/buzzz-frontend/src/user/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useState,useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 import(useState)
 
@@ -12,12 +13,17 @@ const Dashboard = () => {
             setUser(JSON.parse(storedUser))
         }
     })
+    const handleLogout =() =>{
+        localStorage.removeItem("user");
+        Navigate("/login");
+    }
 
     return <div className="dashboard" >
         <h2>
             Welcome to your dashboard{user ? user : "User"}!
         </h2>
         <p>This is your dashboard where you can manage your appointements.</p>
+        <button onClick={handleLogout}> Logout</button>
     </div>
 }
 
